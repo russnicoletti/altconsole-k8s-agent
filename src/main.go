e4ff8958dc742f917b48632ae8ab296133d86124
Russ Nicoletti <russnicoletti@yahoo.com>
2023-03-24 17:11:43 -0700

    fixed bug causing agent to exit immediately

--- src/main.go
+++ src/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	customcontrollers "altc-agent/controllers"
+	"context"
+	"fmt"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
 func main() {
@@ -23,4 +26,24 @@ func main() {
 
 	controller := customcontrollers.New(clientset)
 	controller.Run(stopCh)
+
+	ctx := signals.SetupSignalHandler()
+	err = run(ctx)
+	fmt.Println("agent ending with", err)
+}
+
+func run(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			// Documentation
+			// If Done is not yet closed, Err returns nil.
+			// If Done is closed, Err returns a non-nil error explaining why:
+			//  Canceled if the context was canceled
+			// or
+			//  DeadlineExceeded if the context's deadline passed.
+			return ctx.Err()
+		default:
+		}
+	}
 }

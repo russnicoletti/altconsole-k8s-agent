94cb341b8e7931456613d5633ef9faf375134043
Russ Nicoletti <russnicoletti@yahoo.com>
2023-03-28 16:54:37 -0700

    cleanup old files

--- make.sh
+++
@@ -1,6 +0,0 @@
-set -v
-GOOS=linux go build -C src -o ../altc-agent-linux .
-docker build -t russnicoletti/altc-agent-linux .
-docker tag russnicoletti/altc-agent-linux:latest russnicolettidocker/altc-agent-linux:latest
-docker push russnicolettidocker/altc-agent-linux:latest
-

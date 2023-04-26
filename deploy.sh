ea7d46435687678f8a0cc57d4362524253a833a7
Russ Nicoletti <russnicoletti@yahoo.com>
2023-04-26 10:50:15 -0700

    ALTC-313 Conistency with configuration files

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-ALTC_ENV_CONFIG_MAP_NAME="altc-agent"
-ALTC_ENV_SECRET_NAME="altc-agent"
+ALTC_CONFIG_MAP_NAME="altc-agent"
+ALTC_SECRET_NAME="altc-agent"
 
-kubectl get configmap $ALTC_ENV_CONFIG_MAP_NAME > /dev/null 2>&1
+kubectl get configmap $ALTC_CONFIG_MAP_NAME > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
-  echo "ConfigMap '${ALTC_ENV_CONFIG_MAP_NAME}' already exists"
+  echo "ConfigMap '${ALTC_CONFIG_MAP_NAME}' already exists"
 else
-  echo "ConfigMap '${ALTC_ENV_CONFIG_MAP_NAME}' does not exist. Creating ..."
-  kubectl create configmap $ALTC_ENV_CONFIG_MAP_NAME --from-literal=CLUSTER_NAME=`kubectl config view --minify -o jsonpath='{.clusters[].name}'`
+  echo "ConfigMap '${ALTC_CONFIG_MAP_NAME}' does not exist. Creating ..."
+  kubectl create configmap $ALTC_CONFIG_MAP_NAME --from-literal=CLUSTER_NAME=`kubectl config view --minify -o jsonpath='{.clusters[].name}'`
 fi
 
 echo "${ALTC_CONFIG_MAP_NAME} ConfigMap contents:"
 echo ""
 minikube kubectl -- describe configmap $ALTC_CONFIG_MAP_NAME | grep -A 2 "CLUSTER_NAME"
 
-kubectl get secret $ALTC_ENV_SECRET_NAME > /dev/null 2>&1
+kubectl get secret $ALTC_SECRET_NAME > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
-  echo "secret '${ALTC_ENV_SECRET_NAME}' already exists"
+  echo "secret '${ALTC_SECRET_NAME}' already exists"
 else
-  echo "secret'${ALTC_ENV_SECRET_NAME}' does not exist. Creating ..."
+  echo "secret'${ALTC_SECRET_NAME}' does not exist. Creating ..."
   kubectl apply -f altc-agent-secret.yaml
 fi
 

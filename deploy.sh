f61b2fa04657a5f5ec0090df3e1bc8c3db6eaa6c
Russ Nicoletti <russnicoletti@yahoo.com>
2023-05-03 16:55:41 -0700

    ALTC 314 Additional informers

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,25 @@
 ALTC_CONFIG_MAP_NAME="altc-agent"
 ALTC_SECRET_NAME="altc-agent"
 ALTC_CONFIG_MAP_TEMPLATE_FILE="deployment_files/altc-agent-configmap-template.yaml"
+ALTC_CONFIG_MAP_FILE="altc-agent-configmap.yaml"
 
 kubectl get configmap $ALTC_CONFIG_MAP_NAME > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
   echo "ConfigMap '${ALTC_CONFIG_MAP_NAME}' already exists"
 else
   echo "ConfigMap '${ALTC_CONFIG_MAP_NAME}' does not exist. Creating ..."
+  # Update cluster name in configmap
   CLUSTER_NAME=`kubectl config view --minify -o jsonpath='{.clusters[].name}'`
-  sed "s/REPLACE_WITH_CLUSTER_NAME/${CLUSTER_NAME}/g" ${ALTC_CONFIG_MAP_TEMPLATE_FILE} | kubectl apply -f -
+  sed "s/REPLACE_WITH_CLUSTER_NAME/\"${CLUSTER_NAME}\"/g" ${ALTC_CONFIG_MAP_TEMPLATE_FILE} > ${ALTC_CONFIG_MAP_FILE}
+
+  # Update batch limit in config map template
+  # TODO: allow batch limit to be specified on command line
+  sed -I sav "s/REPLACE_WITH_BATCH_LIMIT/\"15\"/g" ${ALTC_CONFIG_MAP_FILE}
+  kubectl apply -f ${ALTC_CONFIG_MAP_FILE}
 fi
 
 echo "${ALTC_CONFIG_MAP_NAME} ConfigMap contents:"
-kubectl describe configmap $ALTC_CONFIG_MAP_NAME | grep -A 2 "CLUSTER_NAME"
+kubectl describe configmap $ALTC_CONFIG_MAP_NAME | egrep -A 2 'CLUSTER_NAME|BATCH_LIMIT'
 
 kubectl get secret $ALTC_SECRET_NAME > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
--- src/controllers/controller.go
+++ src/controllers/controller.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -20,6 +22,10 @@ type Controller struct {
 	batchLimit        int
 }
 
+const (
+	batchLimitEnv = "BATCH_LIMIT"
+)
+
 func New(clientset *kubernetes.Clientset, clusterName string) *Controller {
 	// Documentation
 	//  The second argument is how often this informer should perform a resync.
@@ -31,15 +37,40 @@ func New(clientset *kubernetes.Clientset, clusterName string) *Controller {
 	f := informers.NewSharedInformerFactory(clientset, 0)
 
 	// TODO make this configurable
-	batchLimit := 5
+	batchLimit, _ := strconv.Atoi(os.Getenv(batchLimitEnv))
 
 	//goland:noinspection SpellCheckingInspection
 	resourceObjectsQ := altcqueues.NewResourceObjectQ()
 	clusterResourceQ := altcqueues.NewClusterResourcesQ(clusterName, batchLimit, resourceObjectsQ)
 
 	custominformers := []*custominformers.Informer{
+		custominformers.New(f.Core().V1().ConfigMaps().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().Endpoints().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().Events().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().LimitRanges().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().Namespaces().Informer(), resourceObjectsQ),
 		custominformers.New(f.Core().V1().Nodes().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().PersistentVolumeClaims().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().PodTemplates().Informer(), resourceObjectsQ),
 		custominformers.New(f.Core().V1().Pods().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().ReplicationControllers().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().ResourceQuotas().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().Secrets().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().ServiceAccounts().Informer(), resourceObjectsQ),
+		custominformers.New(f.Core().V1().Services().Informer(), resourceObjectsQ),
+		custominformers.New(f.Apps().V1().Deployments().Informer(), resourceObjectsQ),
+		custominformers.New(f.Apps().V1().DaemonSets().Informer(), resourceObjectsQ),
+		custominformers.New(f.Apps().V1().ReplicaSets().Informer(), resourceObjectsQ),
+		custominformers.New(f.Apps().V1().StatefulSets().Informer(), resourceObjectsQ),
+		custominformers.New(f.Batch().V1().CronJobs().Informer(), resourceObjectsQ),
+		custominformers.New(f.Batch().V1().Jobs().Informer(), resourceObjectsQ),
+		custominformers.New(f.Networking().V1().Ingresses().Informer(), resourceObjectsQ),
+		custominformers.New(f.Networking().V1().NetworkPolicies().Informer(), resourceObjectsQ),
+		custominformers.New(f.Rbac().V1().ClusterRoles().Informer(), resourceObjectsQ),
+		custominformers.New(f.Rbac().V1().ClusterRoleBindings().Informer(), resourceObjectsQ),
+		custominformers.New(f.Rbac().V1().Roles().Informer(), resourceObjectsQ),
+		custominformers.New(f.Rbac().V1().RoleBindings().Informer(), resourceObjectsQ),
+		custominformers.New(f.Storage().V1().CSIStorageCapacities().Informer(), resourceObjectsQ),
 	}
 
 	return &Controller{
@@ -48,6 +79,7 @@ func New(clientset *kubernetes.Clientset, clusterName string) *Controller {
 		resourceObjectsQ:  resourceObjectsQ,
 		clusterResourcesQ: clusterResourceQ,
 		altcClient:        altc.NewClient(),
+		batchLimit:        batchLimit,
 	}
 }
 
@@ -56,6 +88,7 @@ func (c *Controller) Run(stopCh <-chan struct{}, ctx context.Context) {
 	fmt.Println("controller running")
 	fmt.Println(fmt.Sprintf("cluster name: %s", c.clusterResourcesQ.GetClusterName()))
 	fmt.Println("starting informers...")
+	fmt.Println("batch limit:", c.batchLimit)
 	fmt.Println("****")
 	c.informerFactory.Start(stopCh) // runs in background
 
